81a4cc5ef250b5ee191dbd8b937bfd2b6b75c2e6
Anast0sia <anastypol33@gmail.com>
2024-08-04 11:59:23 +0300

    add implemented

--- main/java/org/example/PhoneBook.java
+++ main/java/org/example/PhoneBook.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    public Map<String, Long> map = new HashMap<>();
+    public int count = 0;
+
     public int add(String name, long number) {
-        return 0;
+        if (!map.containsKey(name)) {
+            map.put(name, number);
+            count++;
+        }
+        return count;
     }
 }
